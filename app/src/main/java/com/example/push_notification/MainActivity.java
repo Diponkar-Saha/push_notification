7f3364cde9a6bc190632d96c2f7dc6e303a2755e
Diponkar Saha <44252172+Diponkar-Saha@users.noreply.github.com>
2020-10-20 01:42:50 +0600

    frist_v

--- app/src/main/java/com/example/push_notification/MainActivity.java
+++ app/src/main/java/com/example/push_notification/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.push_notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    //1.notification channel
+    //2.notification builder
+    //3.notification manager
+    private static final String CHANNEL_ID="diponkar_saha";
+    private static final String CHANNEL_Name="diponkar saha";
+    private static final String CHANNEL_DESC="diponkar_saha_notification";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel=new NotificationChannel(CHANNEL_ID,CHANNEL_Name,NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+        }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+
+    }
+    private void displayNotification(){
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_message_24)
+                .setContentTitle("Hurry its working")
+                .setContentText("wellcome")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat=
+                NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1,builder.build());
     }
 }
